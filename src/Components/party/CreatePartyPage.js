de6388b218f89321900a3dc5cb2d87e5494fa4f5
Pakhomov Aleksandr <aleksandr.pakhomov@sbb.ch>
2020-11-14 19:16:57 +0300

    Optimize imports

--- src/Components/party/CreatePartyPage.js
+++ src/Components/party/CreatePartyPage.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import {Button} from '@material-ui/core';
-import Divider from '@material-ui/core/Divider';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from "@material-ui/core/InputLabel";
-import Input from "@material-ui/core/Input";
-import FormHelperText from "@material-ui/core/FormHelperText";
 import CreatePartyForm from "./CreatePartyForm";
 
 function CreatePartyPage(props) {
     return (
         <Container className="Hello-page">
-            <CreatePartyForm />
+            <CreatePartyForm/>
         </Container>
     );
 }
