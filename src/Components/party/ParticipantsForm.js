5872091d27cf51a7e8e22cd15fd2ff62a9f922d4
Pakhomov Aleksandr <aleksandr.pakhomov@sbb.ch>
2020-11-15 11:32:14 +0300

    Change spacing in forms

--- src/Components/party/ParticipantsForm.js
+++ src/Components/party/ParticipantsForm.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import {Button} from "@material-ui/core";
 
 export default function ParticipantsForm() {
@@ -12,11 +10,11 @@ export default function ParticipantsForm() {
             <Typography variant="h6" gutterBottom>
                 Участники
             </Typography>
-            <Grid container spacing={3}>
-                <Grid item xs={12} >
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
                     <TextField required id="participantName" label="Участник" fullWidth/>
                 </Grid>
-                <Grid item xs={12} >
+                <Grid item xs={12}>
                     <Button variant="contained" color="secondary">
                         Добавить участника
                     </Button>
--- src/Components/party/PartyForm.js
+++ src/Components/party/PartyForm.js
@@ -13,7 +13,7 @@ export default function PartyForm(props) {
             <Typography variant="h6" gutterBottom>
                 Пати
             </Typography>
-            <Grid container spacing={3}>
+            <Grid container spacing={2}>
                 <Grid item xs={12} sm={6}>
                     <TextField
                         required
