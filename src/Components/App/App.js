30bcaa2a28495316fc26d20be11689f8c3482626
Pakhomov Aleksandr <aleksandr.pakhomov@sbb.ch>
2020-11-15 11:29:17 +0300

    Move app bar to App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,15 +3,30 @@ import HelloPage from '../hello/HelloPage'
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import CreatePartyPage from "../party/CreatePartyPage";
 import Copyright from "./Copyright";
-// function App() {
-//     return (
-//         <HelloPage logo={logo}/>
-//     );
-// }
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        position: 'relative',
+    }
+}));
 
 const App = () => {
+    const classes = useStyles();
     return (
         <React.Fragment>
+            <CssBaseline/>
+            <AppBar position="absolute" color="primary" className={classes.appBar}>
+                <Toolbar>
+                    <Typography variant="h6" color="inherit" noWrap>
+                        Party Part
+                    </Typography>
+                </Toolbar>
+            </AppBar>
             <section className="App">
                 <Router>
                     <Route exact path="/" component={HelloPage}/>
--- src/Components/party/CreatePartyForm.js
+++ src/Components/party/CreatePartyForm.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Paper from '@material-ui/core/Paper';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
@@ -15,9 +12,6 @@ import Review from './Review';
 import DutiesForm from "./DutiesForm";
 
 const useStyles = makeStyles((theme) => ({
-    appBar: {
-        position: 'relative',
-    },
     layout: {
         width: 'auto',
         marginLeft: theme.spacing(2),
@@ -120,14 +114,6 @@ export default function CreatePartyForm() {
 
     return (
         <React.Fragment>
-            <CssBaseline/>
-            <AppBar position="absolute" color="primary" className={classes.appBar}>
-                <Toolbar>
-                    <Typography variant="h6" color="inherit" noWrap>
-                        Party Part
-                    </Typography>
-                </Toolbar>
-            </AppBar>
             <main className={classes.layout}>
                 <Paper className={classes.paper}>
                     <Typography component="h1" variant="h4" align="center">
